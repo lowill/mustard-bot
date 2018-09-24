224d7eff6d477156796895744ca57893a5a8da8e
William Lo <lowill.cs@gmail.com>
2018-09-24 00:04:03 -0400

    exclude say command from help

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ const { prefix } = require('../config.json');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 const helpData = {};
-const ignoreList = ['help.js', 'set-presence.js'];
+const ignoreList = ['help.js', 'set-presence.js', 'say.js'];
 
 
 for(const file of commandFiles) {
